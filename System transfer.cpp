50a05c65479e4202edcd27fa684f29fde203115a
NihilSherrKhaine <eridusnihilxorn@gmail.com>
2017-10-21 19:46:05 +0300

    Created. (january 2017)
    Transfer a number from X-scale to Y-scale of notation. Provides transfer for integers and float numbers.

---
+++ System transfer.cpp
@@ -0,0 +1,48 @@
+// System transfer.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int evenPart, system, remaining, counter = -1, limit;
+	float fractionalPart;
+	char output[30];
+	puts("Enter number.");
+	scanf_s("%f", &fractionalPart);
+	puts("Enter system.");
+	scanf_s("%d", &system);
+	puts("What accuracy do you require(amount of numerals after '.')?");
+	scanf_s("%d", &limit);
+	evenPart = fractionalPart;
+	fractionalPart -= evenPart;
+	while (evenPart != 0)
+	{
+		counter++;
+		remaining = evenPart % system;
+		evenPart /= system;
+		if (system < 10 || remaining < 10)
+			output[counter] = (char)(remaining + '0');
+		if (system > 10 && remaining >= 10)
+			output[counter] = (char)('A' - 10 + remaining);
+	}
+	for (int i = counter; i >= 0; i--, counter--)
+		printf_s("%c", output[i]);
+	printf_s(".");
+	while (fractionalPart != 0)
+	{
+		counter++;
+		if (counter == limit - 1)
+			break;
+		remaining = fractionalPart * system;
+		fractionalPart = fractionalPart*system - remaining;
+		if (system < 10 || remaining < 10)
+			printf_s("%d", remaining);
+		if (system > 10 && remaining >= 10)
+			printf_s("%c", (char)('A' - 10 + remaining));
+	}
+	puts("\nDone.");
+    return 0;
+}
+
